7eb90f24ed6ac353effaaf91869ee76929de3474
comey <975008777@qq.com>
2023-04-11 15:21:58 +0800

    feat:删除引入

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { store, persistor } from "@/redux/index";
-// import login from '@/views/login'
 import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

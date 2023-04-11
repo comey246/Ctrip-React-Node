832e33c01f037201a5b05cdced7a738bc64c6c8a
comey <975008777@qq.com>
2023-04-11 15:20:43 +0800

    feat:新建登录表单

---
+++ src/components/LoginForm/index.jsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react'
+
+export default function index() {
+  return (
+    <Fragment>
+        登陆页面<br/>
+        <input type="text" />
+        <br/>
+        <input type="password" />
+        <br/>
+        <button>登录</button>
+    </Fragment>
+ )
+}

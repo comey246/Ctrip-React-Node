a6f704b6546e95e0666c071ded9e9029d977f66b
comey <975008777@qq.com>
2023-04-11 23:11:35 +0800

    feat:login

--- src/views/Login/index.jsx
+++ src/views/Login/index.jsx
@@ -1,29 +1,31 @@
 import React, { Fragment } from "react";
-import { connect } from "react-redux";
+// import { connect } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { Button, Form, Input, Space, Table, Tag } from "antd";
 import LoginForm from "@/views/Login/LoginForm";
 import RegForm from "@/views/Login/regForm/index.jsx";
 import "./index.less";
+import loginLeft from "./images/login_left.png";
+import logo from "./images/logo.png";
 
 export default function index() {
   return (
-    <Fragment>
-      <div className="login">
-        <div className="login-box">
-          <div className="login-left">
-            <img src={loginLeft} alt="login" />
-          </div>
-          <div className="login-form">
-            <div className="login-logo">
-              <img className="login-icon" src={logo} alt="logo" />
-              <span className="logo-text">Hooks-Admin</span>
+      <Fragment>
+        <div className="login-container">
+          <div className="login-box">
+            <div className="login-left">
+              < img src={loginLeft} alt="login" />
             </div>
-            <LoginForm />
-            {/* <RegForm /> */}
+            <div className="login-form">
+              <div className="login-logo">
+                < img className="login-icon" src={logo} alt="logo" />
+                <span className="logo-text">登陆页面</span>
+              </div>
+              <LoginForm />
+              {/* <RegForm /> */}
+            </div>
           </div>
         </div>
-      </div>
-    </Fragment>
+      </Fragment>
   );
 }

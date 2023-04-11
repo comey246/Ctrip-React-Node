b563f8d9a6dddbf2c2dff3d87a8970b70ae8970a
comey <975008777@qq.com>
2023-04-11 19:42:45 +0800

    feat：新建api

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,11 @@
 import axios, { AxiosInstance, AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
+
+const axiosCanceler = new AxiosCanceler();
+const config = {
+	// 默认地址请求地址，可在 .env 开头文件中修改
+	// baseURL: import.meta.env.VITE_API_URL as string,
+	// // 设置超时时间（10s）
+	// timeout: 10000,
+	// // 跨域时候允许携带凭证
+	// withCredentials: true
+};

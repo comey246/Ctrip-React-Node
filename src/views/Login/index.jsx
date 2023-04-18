c52944fe6cd64a2647f1d32a3476d404f1a1c5c6
violet <1269216728@qq.com>
2023-04-18 20:18:48 +0800

    登录页面

--- src/views/Login/index.jsx
+++ src/views/Login/index.jsx
@@ -1,18 +1,29 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from "react";
 import { connect } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { Button, Form, Input,Space, Table, Tag } from 'antd';
-import LoginForm from '@/views/Login/LoginForm'
+import { Button, Form, Input, Space, Table, Tag } from "antd";
+import LoginForm from "@/views/Login/LoginForm";
 import RegForm from "@/views/Login/regForm/index.jsx";
-import './index.css'
+import "./index.less";
 
 export default function index() {
   return (
     <Fragment>
-        <div className="login">
-        <LoginForm/>
-            <RegForm/>
+      <div className="login">
+        <div className="login-box">
+          <div className="login-left">
+            <img src={loginLeft} alt="login" />
+          </div>
+          <div className="login-form">
+            <div className="login-logo">
+              <img className="login-icon" src={logo} alt="logo" />
+              <span className="logo-text">Hooks-Admin</span>
+            </div>
+            <LoginForm />
+            {/* <RegForm /> */}
+          </div>
         </div>
+      </div>
     </Fragment>
-    )
+  );
 }

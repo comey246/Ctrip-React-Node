13cc16766b9dc40097735791be5af2757cf7b53f
violet <1269216728@qq.com>
2023-04-18 21:33:17 +0800

    填充整个页面

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -2,7 +2,7 @@ import React, { Fragment, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { updateCollapse } from "@/redux/menu/action";
 import { connect } from "react-redux";
-import {Layout, Space, theme} from "antd";
+import { Layout, Space, theme } from "antd";
 import LayoutMenu from "./Menu";
 import LayoutFooter from "./Footer";
 import LayoutHeader from "./Header";
@@ -11,70 +11,71 @@ import "./index.css";
 const { Header, Footer, Sider, Content } = Layout;
 
 const Index = (props) => {
-    const {
-        token: { colorBgContainer },
-    } = theme.useToken();
-    const { isCollapse,updateCollapse } = props;
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+  const { isCollapse, updateCollapse } = props;
   return (
     <Fragment>
-      <Space
+      {/* <Space
         direction="vertical"
         style={{
           width: "100%",
         }}
         size={[0, 48]}
-      >
-        <Layout>
-            <Sider trigger={null}
-                   breakpoint="xs"
-                   onBreakpoint={(broken) => {
-                       // console.log(broken);
-                       updateCollapse(broken)
-                   }}
-                   // onCollapse={(collapsed, type) => {
-                   //     console.log(collapsed, type);
-                   //     updateCollapse(collapsed)
-                   // }}
-                   collapsed={isCollapse}>
-
-            <LayoutMenu />
+      > */}
+      <Layout style={{ minHeight: "100%" }}>
+        <Sider
+          trigger={null}
+          breakpoint="xs"
+          onBreakpoint={(broken) => {
+            // console.log(broken);
+            updateCollapse(broken);
+          }}
+          // onCollapse={(collapsed, type) => {
+          //     console.log(collapsed, type);
+          //     updateCollapse(collapsed)
+          // }}
+          collapsed={isCollapse}
+        >
+          <LayoutMenu />
         </Sider>
-          <Layout className="site-layout">
-              <Header
-                  style={{
-                      padding: 0,
-                      background: colorBgContainer,
-                  }}
-              ><LayoutHeader />
-              </Header>
-            <Content
-              style={{
-                margin: "24px 16px",
-                padding: 24,
-                minHeight: 280,
-                background: colorBgContainer,
-              }}
-            >
-              <Outlet />
-            </Content>
-            <Footer
-              style={{
-                margin: "24px 16px",
-                padding: 24,
-                minHeight: 200,
-                background: colorBgContainer,
-              }}
-            >
-              <LayoutFooter />
-            </Footer>
-          </Layout>
+        <Layout className="site-layout">
+          <Header
+            style={{
+              padding: 0,
+              background: colorBgContainer,
+            }}
+          >
+            <LayoutHeader />
+          </Header>
+          <Content
+            style={{
+              margin: "24px 16px",
+              padding: 24,
+              minHeight: 280,
+              background: colorBgContainer,
+            }}
+          >
+            <Outlet />
+          </Content>
+          <Footer
+            style={{
+              margin: "24px 16px",
+              padding: 24,
+              minHeight: 200,
+              background: colorBgContainer,
+            }}
+          >
+            <LayoutFooter />
+          </Footer>
         </Layout>
-      </Space>
+      </Layout>
+      {/* </Space> */}
     </Fragment>
   );
 };
 
 const mapStateToProps = (state) => state.menu;
 const mapDispatchToProps = { updateCollapse };
-export default connect(mapStateToProps,mapDispatchToProps)(Index);
-
+export default connect(mapStateToProps, mapDispatchToProps)(Index);

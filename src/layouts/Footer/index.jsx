dc3408eb6b669895bb6ce55ff9d7409a7e2b2f4b
violet <1269216728@qq.com>
2023-04-19 21:21:47 +0800

    feat:footer

--- src/layouts/Footer/index.jsx
+++ src/layouts/Footer/index.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+import "./index.css";
+import { Divider, Space, Typography } from "antd";
+
+const { Text } = Typography;
 
 const Index = () => {
-    return (
-        <div>
-            <h2>这是footer在layout/footer</h2>
-        </div>
-    );
+  return (
+    <div className="footer">
+      <Space>
+        <Text className="footer-item">旅游资讯</Text>
+        <Divider type="vertical" />
+        <Text className="footer-item">加盟合作</Text>
+        <Divider type="vertical" />
+        <Text className="footer-item">关于携程</Text>
+      </Space>
+    </div>
+  );
 };
 
 export default Index;

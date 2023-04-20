416b57e6ed5575e0eb4506e952137b4d4a9e4c99
comey <975008777@qq.com>
2023-04-20 15:03:06 +0800

    feat:header适配手机

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,56 +1,77 @@
-import React, { Fragment, useState } from "react";
-import { Outlet } from "react-router-dom";
-import { updateCollapse } from "@/redux/menu/action";
-import { connect } from "react-redux";
-import { Layout, Space, theme } from "antd";
+import React, {Fragment, useState} from "react";
+import {Outlet} from "react-router-dom";
+import {updateCollapse} from "@/redux/menu/action";
+import {connect} from "react-redux";
+import {Layout, Space, theme,Affix} from "antd";
 import LayoutMenu from "./Menu";
 import LayoutFooter from "./Footer";
 import LayoutHeader from "./Header";
 import "./index.css";
 
-const { Header, Footer, Sider, Content } = Layout;
+const {Header, Footer, Sider, Content} = Layout;
 
 const Index = (props) => {
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
-  const { isCollapse, updateCollapse } = props;
-  return (
-    <Fragment>
-      <Layout style={{ minHeight: "100%" }}>
-        <Sider
-          // className="menu" // Add the menu class here
-          trigger={null}
-          breakpoint="xs"
-          onBreakpoint={(broken) => {
-            updateCollapse(broken);
-          }}
-          collapsed={isCollapse}
-        >
-          <LayoutMenu />
-        </Sider>
-        <Layout className="site-layout">
-          <Header
-            className="fixed-header"
-            style={{ background: colorBgContainer }}
-          >
-            <LayoutHeader />
-          </Header>
-          <Content
-            className="custom-content"
-            style={{ paddingBottom: "60px", background: colorBgContainer }}
-          >
-            <Outlet />
-          </Content>
-          <Footer className="custom-footer, fixed-footer">
-            <LayoutFooter />
-          </Footer>
-        </Layout>
-      </Layout>
-    </Fragment>
-  );
+    const {
+        token: {colorBgContainer},
+    } = theme.useToken();
+    const {isCollapse, updateCollapse} = props;
+    const [top] = useState(0);
+    return (
+        <Fragment>
+            <Layout
+                // style={{ minHeight: "100%" }}
+            >
+                <Sider
+                    // className="menu" // Add the menu class here
+                    trigger={null}
+                    breakpoint="xs"
+                    onBreakpoint={(broken) => {
+                        updateCollapse(broken);
+                    }}
+                    collapsed={isCollapse}
+                    collapsedWidth="60"
+                >
+                    <Affix offsetTop={top}>
+                    <LayoutMenu/>
+                    </Affix>
+                </Sider>
+                <Layout
+                    // className="site-layout"
+                    // style={{transition: 'margin-left 0.2s'}}
+                >
+                    <Header
+                        // className="fixed-header"
+                        style={{
+                            padding: 0,
+                            background: colorBgContainer,
+                        }}
+                    >
+                        <LayoutHeader/>
+                    </Header>
+                    <Content
+                        // className="custom-content"
+                        style={{
+                            // margin: '24px 16px',
+                            padding: 24,
+                            minHeight: 280,
+                            background: colorBgContainer,
+                        }}
+                    >
+                        <Outlet/>
+                    </Content>
+                    <Footer
+                        style={{
+                            padding: 20,
+                        }}
+                    >
+                        <LayoutFooter/>
+                    </Footer>
+                </Layout>
+            </Layout>
+        </Fragment>
+    );
 };
 
 const mapStateToProps = (state) => state.menu;
-const mapDispatchToProps = { updateCollapse };
+const mapDispatchToProps = {updateCollapse};
 export default connect(mapStateToProps, mapDispatchToProps)(Index);

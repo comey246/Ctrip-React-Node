7e123783746f2fc6de92c642a82ba211b3337393
comey <975008777@qq.com>
2023-04-22 06:01:28 +0800

    feat:注释

--- src/views/Mall/PaymentSuccess/index.jsx
+++ src/views/Mall/PaymentSuccess/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./index.css";
-import "font-awesome/css/font-awesome.min.css";
+// import "font-awesome/css/font-awesome.min.css";
 const PaymentSuccess = () => {
   const flightInfo = {
     flightNumber: "CA1234",

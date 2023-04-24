5dc3f71a4a8bcb9939f502c73821fc5ab52785fa
violet <1269216728@qq.com>
2023-04-24 11:37:35 +0800

    feat:首页

--- src/views/Mall/front/index.jsx
+++ src/views/Mall/front/index.jsx
@@ -1,13 +1,5 @@
 import { Carousel } from "antd";
-import "./index.css";
-
-const contentStyle = {
-  height: "160px",
-  color: "#fff",
-  lineHeight: "160px",
-  textAlign: "center",
-  // background: "#364d79",
-};
+import "./index.less";
 
 const TravelPage = () => (
   <div className="wrapper">
@@ -18,15 +10,7 @@ const TravelPage = () => (
           src="https://pic.c-ctrip.com/flight/fuzzy/HET/640.jpg"
           alt="酒店"
         />
-        <h3
-          style={{
-            ...contentStyle,
-            bottom: "5%",
-            fontFamily: "'Microsoft YaHei', sans-serif",
-          }}
-        >
-          酒店
-        </h3>
+        <h3>酒店</h3>
       </div>
       <div className="carousel-item">
         <img
@@ -34,15 +18,7 @@ const TravelPage = () => (
           src="https://pic.c-ctrip.com/flight/fuzzy/DLC/640.jpg"
           alt="机票"
         />
-        <h3
-          style={{
-            ...contentStyle,
-            bottom: "5%",
-            fontFamily: "'Microsoft YaHei', sans-serif",
-          }}
-        >
-          机票
-        </h3>
+        <h3>机票</h3>
       </div>
       <div className="carousel-item">
         <img
@@ -50,15 +26,7 @@ const TravelPage = () => (
           src="https://pic.c-ctrip.com/flight/fuzzy/HAK/640.jpg"
           alt="门票"
         />
-        <h3
-          style={{
-            ...contentStyle,
-            bottom: "5%",
-            fontFamily: "'Microsoft YaHei', sans-serif",
-          }}
-        >
-          门票
-        </h3>
+        <h3>门票</h3>
       </div>
     </Carousel>
   </div>
